db537e5db8d97310471692f9978ab13a101a98ff
Rick Daniel <rick@araishikeiwai.com>
2017-11-03 14:25:39 +0700

    added api routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :accounts
-  resources :mutations
+  constraints subdomain: 'api' do
+    resources :accounts
+    resources :mutations
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
